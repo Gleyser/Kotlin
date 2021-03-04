db1607f51c1352318b015f3e449e2c5868ea6cfd
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2021-03-04 14:36:04 -0300

    Kotlin Android Extensions: Say goodbye to findViewById

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btcalcular = buttonCalcular
+        val resultado = textViewResultado
+        resultado.setText("Aluno aprovado")
+
+
+        }
+
+
     }
-}
